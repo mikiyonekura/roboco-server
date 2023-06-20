3ec1edb8c6811f10a05a404d7d5ea882c9a18705
yonekuramiki <yonekuramiki@yonekuramikinoMacBook-Pro.local>
2023-06-20 20:08:22 +0900

    added timer-naive-func

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, request, abort
 import re
 import datetime
 
-
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -17,6 +16,20 @@ app = Flask(__name__)
 
 line_bot_api = LineBotApi('gl+esNOEr7z+LIRpQarujmD4uPDhHGF4SVtmGK8WjtwSQrsPQONfOdcOpJ2/mpOxA6ZBPhK8xutA88+W59Orz4MH1B5fJihmmhDY8n0KkVXo0uJplGg1tsqJN+NXUPDo4BDLZRA1unMqfkz1upPUsQdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('1f17d0d837d0e1cfa65930e681790687')
+
+
+#ロボ娘を起動する関数
+def boot_robot():
+    print("ロボ娘起動、起きろーー!!(spresenseにHTTP通信を送る)\nロボ娘起動、起きろーー!!(spresenseにHTTP通信を送る)\nロボ娘起動、起きろーー!!(spresenseにHTTP通信を送る)\nロボ娘起動、起きろーー!!(spresenseにHTTP通信を送る)\n")
+
+#設定した時間までの秒数をカウントする関数
+def timer(dif):
+    import time
+    time.sleep(dif)
+    # ロボ娘起動
+    boot_robot()
+
+
 
 @app.route("/")
 def test():
@@ -70,6 +83,8 @@ def handle_message(event):
 
         reply_text="{}時間{}分{}秒経過したよおおお。合計勉強時間は{}秒です".format(int(hour),int(minitue),int(second),int(users[userId]["total"]))
 
+
+    # リッチメニュー「アラーム設定」ボタンを押したときの処理
     elif event.message.text == "アラームを設定":
         reply_text="何時に設定する？"
 
@@ -81,25 +96,26 @@ def handle_message(event):
         minute = match.group(2)
 
         dt_now = datetime.datetime.now()
-        start_sec = dt_now.hour*3600 + dt_now.minute*60
+        start_sec = dt_now.hour*3600 + dt_now.minute*60 + dt_now.second
         end_sec = int(hour)*3600 + int(minute)*60
 
-        dif = end_sec - start_sec
-
+        dif = abs(end_sec - start_sec)
 
         reply_text="{}時{}分に設定したよ.{}秒後に起きればいいってことだ".format(hour,minute,dif)
-
-
 
 
     else:
         reply_text=event.message.text
 
-    
+    #lineにメッセージを送る
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=reply_text))
-
+    
+    #目的の時間までの秒数をカウントする関数
+    timer(dif)
+    dif=0
+    
 
 if __name__ == "__main__":
     app.run()
